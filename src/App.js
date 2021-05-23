d25a80d393692bfd19d00db6b515b494531fa22b
thraddash <theartist@gmail.com>
2021-05-23 16:09:31 -0700

    form add post, update post

--- src/App.js
+++ src/App.js
@@ -5,12 +5,32 @@ import Data from "./data.json";
 function App() {
   return (
     <div className="App">
+      <div>
+        <h4>Add New Post</h4>
+        <input placeholder="Title" />
+        <br />
+        <textarea placeholder="Content"></textarea>
+        <br />
+        <button>Add Post</button>
+      </div>
+
+      <div>
+        <h4>Update Post</h4>
+        <input placeholder="Title" />
+        <br />
+        <textarea placeholder="Content"></textarea>
+        <br />
+        <button>Update Post</button>
+      </div>
+
       <div className="posts">
         { Data.map(post => {
           return(
             <div key={ post.id } className="post">
               <h3>{ post.title } </h3>
               <p>{ post.content }</p>
+              <button>Edit</button>
+              <button>Delete</button>
             </div>
           )
         })}
