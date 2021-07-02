f944da03e65f1d82495d28ddaa396cdba37e718b
thraddash <theartist@gmail.com>
2021-07-01 20:14:20 -0700

    test css

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -8,7 +8,7 @@ const Recipe = ({ recipe }) => {
     return (
         <div className="recipe">
             <h2>{recipe_name}</h2>
-            <img src={image} alt={recipe_name} />
+            <img src={'/images/' + image} alt={recipe_name} />
             <button onClick={() => setShow(!show)}>Ingredients</button>
             {show && <RecipeDetails ingredients={ingredients} />}
         </div>
