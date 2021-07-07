f98e32efa1ca987c4992eb8767c314f8490ef957
thraddash <theartist@gmail.com>
2021-07-06 20:41:15 -0700

    test video

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import DisplayVideos from "../components/DisplayVideos";
 import ".././App.css";
 
 function Videos() {
   return (
     <div className='videos'>
       <h1>Videos</h1>
+      <DisplayVideos />
     </div>
   );
 }
