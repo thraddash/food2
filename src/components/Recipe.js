be99a05404e1097d4d272a0861b05b177d8db61a
thraddash <theartist@gmail.com>
2021-07-07 21:59:11 -0700

    update ingredient dropdown

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,14 +3,14 @@ import RecipeDetails from './RecipeDetails'
 
 const Recipe = ({ recipe }) => {
     const [show, setShow] = useState(false)
-    const { recipe_name, image, ingredients } = recipe;
+    const { recipe_name, image, ingredients, direction, note } = recipe;
 
     return (
         <div className="recipe">
             <h2>{recipe_name}</h2>
             <img src={'/images/' + image} alt={recipe_name} />
             <button onClick={() => setShow(!show)}>Ingredients</button>
-            {show && <RecipeDetails ingredients={ingredients} />}
+            {show && <RecipeDetails ingredients={ingredients} direction={direction} note={note}/>}
         </div>
     );
 };
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Divider from '@material-ui/core/Divider';
 import { v4 as uuidv4 } from 'uuid';
 
-const RecipeDetails = ({ ingredients }) => {
+const RecipeDetails = ({ingredients, direction, note}) => {
   /* return ingredients.map(ingredient => { */
     return (
       <div key={uuidv4()} className="ingredient-list">
-        <div className="ingredient-text">{ingredients} </div>
+        <div className="ingredient-text"><h5>Ingredients:</h5>{ingredients} </div><br/>
+        <Divider/>
+        <div className="ingredient-text"><h5>Directions:</h5>{direction} </div><br/>
+        <Divider/>
+        <div className="ingredient-text"><h5>Notes:</h5>{note} </div><br/>
       </div>
     );
  /*  }); */
