527dbcee6742d8dfa6a908cdc53dc08a467dd5e8
theartist <theartist@gmail.com>
2021-09-09 01:18:29 -0700

    Update RecipeDetails.js

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -4,11 +4,11 @@ import { v4 as uuidv4 } from 'uuid';
 import ImageAccordion from './Accordion/ImageAccordion';
 import VideoAccordion from './Accordion/VideoAccordion';
 
-const RecipeDetails = ({ ingredients, direction, note, misc_image, misc_video }) => {
+const RecipeDetails = ({ ingredient, direction, note, misc_image, misc_video }) => {
 
   return (
     <div key={uuidv4()} className="ingredient-list">
-      <div className="ingredient-text"><h5>Ingredients:</h5>{ingredients} </div><br />
+      <div className="ingredient-text"><h5>Ingredients:</h5>{ingredient} </div><br />
       <Divider />
       <div className="ingredient-text"><h5>Directions:</h5>{direction} </div><br />
       <Divider />
