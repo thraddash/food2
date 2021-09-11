9d1c89e583a68596cf1dba871e3073d51299e696
thraddash <theartist@gmail.com>
2021-09-11 15:39:25 -0700

    update path

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ docker build . -t food2:latest
 
 docker stop food2
 
-docker run -v $PWD/../backup/images:/app/public/images -v $PWD/../backup/videos:/app/public/videos -v $PWD/../backup/data:/app/src/data -p 3000:3000 -p 5000:5000 -d --rm --name food2 food2
+cd ../
+
+docker run -v $PWD/backup/images:/app/public/images -v $PWD/backup/videos:/app/public/videos -v $PWD/backup/data:/app/src/data -p 3000:3000 -p 5000:5000 -d --rm --name food2 food2
