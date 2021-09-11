fb6cd80ded07bed1a42b1e4e0a3372b6bb0da990
thraddash <theartist@gmail.com>
2021-09-11 02:14:24 -0700

    update mount path to /src/data

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker build . -t food2:latest
 
 docker stop food2
 
-docker run -v $PWD/public:/app/public -v $PWD/src:/app/src -p 3000:3000 -p 5000:5000 -d --rm --name food2 food2
+docker run -v $PWD/public:/app/public -v $PWD/src/data:/app/src/data -p 3000:3000 -p 5000:5000 -d --rm --name food2 food2
