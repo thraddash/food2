20c1ef8a8a6871f058faac024dfbf6bdd006b44c
thraddash <theartist@gmail.com>
2021-09-12 02:46:46 -0700

    run.sh creates .env for frontend and runs docker-compose build

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker build . -t food2:latest
+echo "REACT_APP_LOCALHOST = $(hostname -I | awk '{print $1}')" > frontend/.env
+docker-compose build
 
-docker stop food2
-
-cd ../
-
-docker run -v $PWD/backup/images:/app/public/images -v $PWD/backup/videos:/app/public/videos -v $PWD/backup/data:/app/src/data -p 3000:3000 -p 5000:5000 -d --rm --name food2 food2
+#docker build . -t food2:latest
+#docker stop food2
+#cd ../
+#docker run -v $PWD/backup/images:/app/public/images -v $PWD/backup/videos:/app/public/videos -v $PWD/backup/data:/app/src/data -p 3000:3000 -p 5000:5000 -d --rm --name food2 food2
